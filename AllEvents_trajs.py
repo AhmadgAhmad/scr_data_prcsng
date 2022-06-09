aa4d989e8298db4e28782cf487f008a265698180
AhmadgAhmad <ahmadgh@bu.edu>
2022-06-09 10:52:36 -0400

    TODO add the players who are involved in the event

--- AllEvents_trajs.py
+++ AllEvents_trajs.py
@@ -7,6 +7,7 @@ and extract the corresponding trajectory for each event from the trajectory data
 
 For each datapoint create a ['Label','Feature'] pairs, where
 - Label = 'Team name'+'_'+'Event Type'+'_'+'event subtype if exists'
+- plyr_ids = [plyr 1]
 - Feature = [[(x_p1,y_p1,speed_p1,heading_p1),...,(x_p11,y_p11,speed_p11,heading_p11)],
 [(x_p12,y_p12,speed_p12,heading_p12),...,(x_p22,y_p22,speed_p22,heading_p22)],[(x_ball,y_ball,theta_ball,v_ball,ball)]]: 
 23 X 1 list of players and the ball trajectories and heading, where
